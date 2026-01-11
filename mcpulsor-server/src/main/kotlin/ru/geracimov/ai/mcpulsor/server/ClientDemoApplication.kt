42e7221e8e70310f8429a725ebb6b16e86690f53
geracimov <geracimov@gmail.com>
2026-01-11 14:58:20 +0300

    feat(server): добавьте демо-приложение клиента MCP

---
+++ mcpulsor-server/src/main/kotlin/ru/geracimov/ai/mcpulsor/server/ClientDemoApplication.kt
@@ -0,0 +1,26 @@
+package ru.geracimov.ai.mcpulsor.server
+
+import io.modelcontextprotocol.client.McpClient
+import io.modelcontextprotocol.client.transport.HttpClientStreamableHttpTransport
+import io.modelcontextprotocol.spec.McpSchema
+
+fun main() {
+
+    val clientTransport = HttpClientStreamableHttpTransport
+        .builder("http://localhost:8091")
+        .endpoint("/mcpulsor")
+        .build()
+
+    with(McpClient.sync(clientTransport).build()) {
+        initialize()
+        println("Tool list:")
+        listTools().tools.forEach { println(it) }
+        println("------------------")
+
+        callTool(
+            McpSchema.CallToolRequest.builder().name("bioSensor").build()
+        ).content.forEach { println("Результат: $it") }
+    }
+
+
+}
